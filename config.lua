1022c90eb4157f7add69529b5247f8ffe0318f58
smitthyy <adamjchapman@pm.me>
2022-09-29 22:45:55 +0100

    updated to match live server

--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 Config.MinimalDoctors = 2 -- How many players with the ambulance job to prevent the hospital check-in system from being used
 Config.DocCooldown = 1 -- Cooldown between doctor calls allowed, in minutes
 Config.WipeInventoryOnRespawn = true -- Enable or disable removing all the players items when they respawn at the hospital
-Config.Helicopter = "polmav" -- Helicopter model that players with the ambulance job can use
-Config.BillCost = 2000 -- Price that players are charged for using the hospital check-in system
+Config.Helicopter = "md902" -- Helicopter model that players with the ambulance job can use
+Config.BillCost = 1500 -- Price that players are charged for using the hospital check-in system
 Config.DeathTime = 300 -- How long the timer is for players to bleed out completely and respawn at the hospital
 Config.PainkillerInterval = 60 -- Set the length of time painkillers last (per one)
 Config.HealthDamage = 5 -- Minumum damage done to health before checking for injuries
@@ -34,81 +34,124 @@ Config.DamageMinorToMajor = 35 -- How much damage would have to be applied for a
 Config.AlertShowInfo = 2 -- How many injuries a player must have before being alerted about them
 
 Config.Locations = { -- Edit the various interaction points for players or create new ones
-    ["checking"] = {
-	    [1] = vector3(308.19, -595.35, 43.29),
-	    [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
-    },
-    ["duty"] = {
-        [1] = vector3(311.18, -599.25, 43.29),
-        [2] = vector3(-254.88, 6324.5, 32.58),
-    },
-    ["vehicle"] = {
-        [1] = vector4(294.578, -574.761, 43.179, 35.79),
-        [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
-    },
-    ["helicopter"] = {
-        [1] = vector4(351.58, -587.45, 74.16, 160.5),
-        [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
-    },
-    ["armory"] = {
-        [1] = vector3(309.93, -602.94, 43.29),
-        [2] = vector3(-245.13, 6315.71, 32.82),
-    },
-    ["roof"] = {
-        [1] = vector4(338.5, -583.85, 74.16, 245.5),
-    },
-    ["main"] = {
-        [1] = vector3(298.74, -599.33, 43.29),
-    },
-    ["stash"] = {
-        [1] = vector3(309.78, -596.6, 43.29),
-    },
-    ["beds"] = {
-        [1] = {coords = vector4(353.1, -584.6, 43.11, 152.08), taken = false, model = 1631638868},
-        [2] = {coords = vector4(356.79, -585.86, 43.11, 152.08), taken = false, model = 1631638868},
-        [3] = {coords = vector4(354.12, -593.12, 43.1, 336.32), taken = false, model = 2117668672},
-        [4] = {coords = vector4(350.79, -591.8, 43.1, 336.32), taken = false, model = 2117668672},
-        [5] = {coords = vector4(346.99, -590.48, 43.1, 336.32), taken = false, model = 2117668672},
-        [6] = {coords = vector4(360.32, -587.19, 43.02, 152.08), taken = false, model = -1091386327},
-        [7] = {coords = vector4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327},
-        [8] = {coords = vector4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327},
-	--- paleto
-	    [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
-        [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
-        [11] = {coords = vector4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672},
-    },
-    ["jailbeds"] = {
-        [1] = {coords = vector4(1761.96, 2597.74, 45.66, 270.14), taken = false, model = 2117668672},
-        [2] = {coords = vector4(1761.96, 2591.51, 45.66, 269.8), taken = false, model = 2117668672},
-        [3] = {coords = vector4(1771.8, 2598.02, 45.66, 89.05), taken = false, model = 2117668672},
-        [4] = {coords = vector4(1771.85, 2591.85, 45.66, 91.51), taken = false, model = 2117668672},
-    },
-    ["stations"] = {
-        [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(304.27, -600.33, 43.28, 272.249)}
-    }
+["checking"] = {
+    [1] = vector3(308.68, -592.09, 43.28),
+    [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
+},
+["duty"] = {
+    [1] = vector3(351.23, -588.25, 28.8),
+    [2] = vector3(-254.88, 6324.5, 32.58),
+    [3] = vector3(-1419.29, -259.59, 46.48),
+},
+["vehicle"] = {
+    [1] = vector4(326.69, -572.15, 28.8, 339.46),
+    [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
+    [3] = vector4(-1427.13, -259.62, 46.48, 146.86),
+},
+["helicopter"] = {
+    [1] = vector4(351.58, -587.45, 74.16, 160.5),
+    [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
+},
+["armory"] = {
+    [1] = vector3(348.69, -588.0, 28.8),
+    [2] = vector3(-245.13, 6315.71, 32.82),
+    [3] = vector3(-1425.83, -251.11, 46.48),
+},
+["roof"] = {
+    [1] = vector4(338.5, -583.85, 74.16, 245.5),
+},
+["main"] = {
+    [1] = vector3(299.35, -591.45, 43.28),
+},
+["stash"] = {
+    [1] = vector3(309.78, -596.6, 43.29),
+    [2] = vector3(-1433.31, -256.46, 46.48),
+},
+["beds"] = {
+    [1] = {coords = vector4(322.65, -587.46, 44.2, 338.58), taken = false, model = 1631638868},
+    [2] = {coords = vector4(317.74, -585.68, 44.2, 338.35), taken = false, model = 1631638868},
+    [3] = {coords = vector4(314.61, -584.4, 44.2, 338.24), taken = false, model = 1631638868},
+    [4] = {coords = vector4(311.07, -583.08, 44.2, 338.05), taken = false, model = 1631638868},
+    [5] = {coords = vector4(307.82, -581.89, 44.2, 338.82), taken = false, model = 1631638868},
+    [6] = {coords = vector4(309.21, -577.16, 44.2, 153.28), taken = false, model = -1631638868},
+    [7] = {coords = vector4(313.83, -578.93, 44.2, 153.39), taken = false, model = -1631638868},
+    [8] = {coords = vector4(319.33, -580.71, 44.2, 153.15), taken = false, model = -1631638868},
+--- paleto
+    [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
+    [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
+    [11] = {coords = vector4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672},
+},
+["jailbeds"] = {
+    [1] = {coords = vector4(1761.96, 2597.74, 45.66, 270.14), taken = false, model = 2117668672},
+    [2] = {coords = vector4(1761.96, 2591.51, 45.66, 269.8), taken = false, model = 2117668672},
+    [3] = {coords = vector4(1771.8, 2598.02, 45.66, 89.05), taken = false, model = 2117668672},
+    [4] = {coords = vector4(1771.85, 2591.85, 45.66, 91.51), taken = false, model = 2117668672},
+},
+["stations"] = {
+    [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(304.27, -600.33, 43.28, 272.249)},
+    [2] = {label = Lang:t('Ambulance Station'), coords = vector4(-1434.15, -277.35, 46.54, 139.84)}
+}
 }
 
 Config.AuthorizedVehicles = { -- Vehicles players can use based on their ambulance job grade level
-	-- Grade 0
+	-- Grade 0 Trainee
 	[0] = {
-		["ambulance"] = "Ambulance",
+		["paramedic"] = "Van Ambulance",
+        ["ambulance"] = "Box Ambulance",
 	},
-	-- Grade 1
+	-- Grade 1 Paramedic
 	[1] = {
-		["ambulance"] = "Ambulance",
-
+		["paramedic"] = "Van Ambulance",
+        ["ER2"] = "Skoda Octavia RRV",
 	},
-	-- Grade 2
+	-- Grade 2 Adv Paramedic
 	[2] = {
-		["ambulance"] = "Ambulance",
+        ["paramedic"] = "Van Ambulance",
+		["RRV1"] = "VW Tourag Advanced Paramedic",
+        ["ER2"] = "VW Tiguan RRV",
+        ["lasb1"] = "BMW Response Motorcycle",
 	},
-	-- Grade 3
+	-- Grade 3 Doctor
 	[3] = {
-		["ambulance"] = "Ambulance",
+        ["paramedic"] = "Van Ambulance",
+		["assistchief"] = "Skoda Octavia HEMS",
 	},
-	-- Grade 4
+	-- Grade 4 Nurse
 	[4] = {
-		["ambulance"] = "Ambulance",
+        ["paramedic"] = "Van Ambulance",
+		["assistchief"] = "Skoda Octavia HEMS",
+	},
+    -- Grade 5 Surgeon
+    [5] = {
+        ["paramedic"] = "Van Ambulance",
+        ["ambulance"] = "Box Ambulance",
+        ["ER1"] = "VW Tiguan RRV",
+        ["ER2"] = "Skoda Octavia RRV",
+        ["assistchief"] = "Skoda Octavia HEMS",
+	},
+    -- Grade 6 Station Manager
+    [6] = {
+        ["paramedic"] = "Van Ambulance",
+        ["ambulance"] = "Box Ambulance",
+        ["ER1"] = "VW Tiguan RRV",
+        ["ER2"] = "Skoda Octavia RRV",
+        ["RRV1"] = "VW Tourag Advanced Paramedic",
+        ["specilist"] = "BMW X5 Paramedic Supervisor",
+        ["lasb1"] = "BMW Response Motorcycle",
+        ["fastresponders"] = "BMW X5 Hems Support",
+        ["assistchief"] = "Skoda Octavia Hems Support",
+	},
+    -- Grade 7 Director
+    [7] = {
+        ["paramedic"] = "Van Ambulance",
+        ["ambulance"] = "Box Ambulance",
+        ["ER1"] = "VW Tiguan RRV",
+        ["ER2"] = "Skoda Octavia RRV",
+        ["RRV1"] = "VW Tourag Advanced Paramedic",
+        ["specilist"] = "BMW X5 Paramedic Supervisor",
+        ["lasb1"] = "BMW Response Motorcycle",
+        ["fastresponders"] = "BMW X5 Hems Support",
+        ["assistchief"] = "Skoda Octavia Hems Support",
 	}
 }
 
