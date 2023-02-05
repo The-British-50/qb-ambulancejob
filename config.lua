571f4e68b114b78bb5f0ea61efd5bb4d89ea05bc
Smithy <96345069+Smitthyy@users.noreply.github.com>
2023-02-05 20:32:05 +0000

    updated to lj-inventory

--- config.lua
+++ config.lua
@@ -35,16 +35,16 @@ Config.AlertShowInfo = 2 -- How many injuries a player must have before being al
 
 Config.Locations = { -- Edit the various interaction points for players or create new ones
 ["checking"] = {
-    [1] = vector3(308.68, -592.09, 43.28),
+    [1] = vector3(308.19, -595.35, 43.29),
     [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
 },
 ["duty"] = {
-    [1] = vector3(351.23, -588.25, 28.8),
+    [1] = vector3(311.18, -599.25, 43.29),
     [2] = vector3(-254.88, 6324.5, 32.58),
     [3] = vector3(-1419.29, -259.59, 46.48),
 },
 ["vehicle"] = {
-    [1] = vector4(326.69, -572.15, 28.8, 339.46),
+    [1] = vector4(294.578, -574.761, 43.179, 35.79),
     [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
     [3] = vector4(-1427.13, -259.62, 46.48, 146.86),
 },
@@ -53,7 +53,7 @@ Config.Locations = { -- Edit the various interaction points for players or creat
     [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
 },
 ["armory"] = {
-    [1] = vector3(348.69, -588.0, 28.8),
+    [1] = vector3(309.93, -602.94, 43.29),
     [2] = vector3(-245.13, 6315.71, 32.82),
     [3] = vector3(-1425.83, -251.11, 46.48),
 },
@@ -68,14 +68,14 @@ Config.Locations = { -- Edit the various interaction points for players or creat
     [2] = vector3(-1433.31, -256.46, 46.48),
 },
 ["beds"] = {
-    [1] = {coords = vector4(322.65, -587.46, 45.2, 338.58), taken = false, model = 1631638868},
-    [2] = {coords = vector4(317.74, -585.68, 45.2, 338.35), taken = false, model = 1631638868},
-    [3] = {coords = vector4(314.61, -584.4, 45.2, 338.24), taken = false, model = 1631638868},
-    [4] = {coords = vector4(311.07, -583.08, 45.2, 338.05), taken = false, model = 1631638868},
-    [5] = {coords = vector4(307.82, -581.89, 45.2, 338.82), taken = false, model = 1631638868},
-    [6] = {coords = vector4(309.21, -577.16, 45.2, 153.28), taken = false, model = -1631638868},
-    [7] = {coords = vector4(313.83, -578.93, 45.2, 153.39), taken = false, model = -1631638868},
-    [8] = {coords = vector4(319.33, -580.71, 45.2, 153.15), taken = false, model = -1631638868},
+    [1] = {coords = vector4(353.1, -584.6, 43.11, 152.08), taken = false, model = 1631638868},
+    [2] = {coords = vector4(356.79, -585.86, 43.11, 152.08), taken = false, model = 1631638868},
+    [3] = {coords = vector4(354.12, -593.12, 43.1, 336.32), taken = false, model = 2117668672},
+    [4] = {coords = vector4(350.79, -591.8, 43.1, 336.32), taken = false, model = 2117668672},
+    [5] = {coords = vector4(346.99, -590.48, 43.1, 336.32), taken = false, model = 2117668672},
+    [6] = {coords = vector4(360.32, -587.19, 43.02, 152.08), taken = false, model = -1091386327},
+    [7] = {coords = vector4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327},
+    [8] = {coords = vector4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327},
 --- paleto
     [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
     [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
@@ -88,9 +88,7 @@ Config.Locations = { -- Edit the various interaction points for players or creat
     [4] = {coords = vector4(1771.85, 2591.85, 45.66, 91.51), taken = false, model = 2117668672},
 },
 ["stations"] = {
-    [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(304.27, -600.33, 43.28, 272.249)},
-    [2] = {label = Lang:t('Ambulance Station'), coords = vector4(-1434.15, -277.35, 46.54, 139.84)}
-}
+    [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(304.27, -600.33, 43.28, 272.249)},}
 }
 
 Config.AuthorizedVehicles = { -- Vehicles players can use based on their ambulance job grade level
@@ -473,7 +471,7 @@ Config.Weapons = { -- Correspond weapon names to their class number
     [`WEAPON_PISTOL50`] = Config.WeaponClasses['MEDIUM_CALIBER'],
     [`WEAPON_REVOLVER`] = Config.WeaponClasses['MEDIUM_CALIBER'],
     [`WEAPON_REVOLVER_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_SMG`] = Config.WeaponClasses['MEDIUM_CALIBER'],
+    [`WEAPON_SMG`] = Config.WeaponClasses['SMALL_CALIBER'],
     [`WEAPON_SMG_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
     [`WEAPON_SPECIALCARBINE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
     [`WEAPON_SPECIALCARBINE_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
